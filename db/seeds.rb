30cccba2071b31802c7d77379432681ab83cd0ef
Alban Peignier <alban@tryphon.org>
2017-04-17 18:00:39 +0200

    Create users with devise 4.2.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by!(email: 'admin@example.com') do |admin|
+  generated_password = SecureRandom.hex
+  puts "Admin password: #{generated_password}"
+  admin.password = generated_password
+end
